da53d096c5ff0e717026a2df156bd0c4c09a99af
Sathish <sathish.ragupathi.r@gmail.com>
2026-02-12 20:10:41 +0530

    new changes

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   if (isConnected) return;
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_DB_URL);
     isConnected = conn.connections[0].readyState;
     console.log('MongoDB connected');
   } catch (err) {
