fbd376833041688b649eb9f7795f7ec91b326108
Sathish <sathish.ragupathi.r@gmail.com>
2026-02-12 20:25:22 +0530

    new changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
+  if (!process.env.MONGO_DB_URL) {
     console.log('MONGO_URI missing');
     return;
   }
@@ -9,7 +9,5 @@ const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_DB_URL);
   console.log('MongoDB connected');
 };
-
-module.exports = connectDB;
 
 module.exports = connectDB;
